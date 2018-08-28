44a01a40656e09670baa2b19497f1edbdded3f2b
alejandrods <alejandro.diaz.santos@gmail.com>
2018-08-28 07:59:29 +0200

    First Commit

---
+++ src/tools.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "tools.h"
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using std::vector;
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+                              const vector<VectorXd> &ground_truth) {
+  /**
+  TODO:
+    * Calculate the RMSE here.
+  */
+  
+  VectorXd rmse(4);
+  //Initializer rmse vector
+  rmse << 0,0,0,0;
+  
+  if (estimations.size() != ground_truth.size() || estimations.size() == 0)
+  {
+	  return rmse;
+  }
+  
+  for(unsigned int i=0; i < estimations.size(); i++)
+  {
+	  VectorXd rest = estimations[i] - ground_truth[i];
+	  
+	  rest = rest.array()*rest.array();
+	  
+	  rmse += rest;
+	  
+  }
+  rmse = rmse/estimations.size();
+  rmse = sqrt(rmse.array());
+  
+  return rmse;
+  
+}
